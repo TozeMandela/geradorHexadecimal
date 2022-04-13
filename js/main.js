bf61522ee5526a33791e815e292388554bcc6770
TozeMandela <101975093+TozeMandela@users.noreply.github.com>
2022-04-13 12:12:11 +0100

    a

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 ((doc, win) => {
     'use strict';
     var $div = doc.querySelector('[data-js="div"]');
-    var arr = [1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+    var arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
     var hexD =[];
 
     for(var i = 0; i <= 5; i++){
-        var aux=Math.floor(Math.random() * (15 - 0)) + 0;
+        var aux=Math.floor(Math.random() * (16 - 0)) + 0;
         hexD.push(arr[aux]);
     }
     
+    var isBlack = hexD.every((value)=>{
+        return value===0;
+    });
+
+    if(isBlack)
+        $div.style.color = 'white';
     $div.style.backgroundColor = `#${hexD.join('')}`;
     $div.innerHTML = `#${hexD.join('')}`;
 
